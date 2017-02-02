efb2c6a22946f11617756a497bb4f5757a5320f6
gcjavier <carolinajavierenriquez@gmail.com>
2017-02-02 15:27:21 -0500

    avance de la seccion de opiniones

--- javascripts/slider.js
+++ javascripts/slider.js
@@ -81,5 +81,46 @@ $(document).ready(function(){
         // instead of a settings object
       ]
     });
+
+    $('.slider-opinion > .slider').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            fade: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
   })
 });
